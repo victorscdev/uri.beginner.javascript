2a6cd6a322214c46ee9c74d44199d9370cb5643e
Victor Santana Costa <39810026+victorscdev@users.noreply.github.com>
2020-02-04 15:15:01 -0200

    Create 1001.js

---
+++ 1001.js
@@ -0,0 +1,22 @@
+/******************* Descrição do Problema *******************/
+/*
+    Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+    Entrada
+    A entrada contém 2 valores inteiros.
+
+    Saída
+    Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha. Cuide para que tenha um espaço antes e depois do sinal de igualdade.
+*/
+
+
+var A = parseInt( lines.shift() );
+var B = parseInt( lines.shift() );
+
+console.log( "X = " + ( A + B ) );
+
+
+/******************* Observações *******************/
+/*
+    No URI quando usamos javascript deve-se "simular" a entrada do usuário, por isso, não use o método prompt(). sempre use lines.shift() para "simular" a entrada do usuário.
+*/

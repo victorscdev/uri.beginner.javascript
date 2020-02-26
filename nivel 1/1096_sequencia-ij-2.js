cf51dc61bfa6e2d8785d33766ce8edf2711eac32
Victor Santana Costa <39810026+victorscdev@users.noreply.github.com>
2020-02-26 06:26:36 -0300

    Create 1096_sequencia-ij-2.js

---
+++ nivel 1/1096_sequencia-ij-2.js
@@ -0,0 +1,33 @@
+
+
+/******************* Descrição do Problema *******************/
+/*
+    Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+    Entrada
+    Não há nenhuma entrada neste problema.
+
+    Saída
+    Imprima a sequencia conforme exemplo abaixo
+
+/* Variaveis */
+console.log( 'I=1 J=7' );
+console.log( 'I=1 J=6' );
+console.log( 'I=1 J=5' );
+console.log( 'I=3 J=7' );
+console.log( 'I=3 J=6' );
+console.log( 'I=3 J=5' );
+console.log( 'I=5 J=7' );
+console.log( 'I=5 J=6' );
+console.log( 'I=5 J=5' );
+console.log( 'I=7 J=7' );
+console.log( 'I=7 J=6' );
+console.log( 'I=7 J=5' );
+console.log( 'I=9 J=7' );
+console.log( 'I=9 J=6' );
+console.log( 'I=9 J=5' );
+
+/******************* Observações *******************/
+/*
+    No URI quando usamos javascript deve-se "simular" a entrada do usuário, por isso, não use o método prompt(). sempre use lines.shift() para "simular" a entrada do usuário.
+*/
